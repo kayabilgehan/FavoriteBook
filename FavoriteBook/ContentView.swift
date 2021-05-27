28f660b61610993f2f2cf7e352f6a7612c8bde1d
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-05-28 00:12:57 +0300

    Initial Commit

---
+++ FavoriteBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoriteBook
+//
+//  Created by Bilgehan KAYA on 28.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
